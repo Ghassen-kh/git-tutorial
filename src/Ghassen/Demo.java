8c6ddc9d57719156fb0c1f99c6d205d06d658122
Ghassen Khalouaoui <ghassen.khalouaoui15@gmail.com>
2023-02-12 15:28:25 +0100

    init commit

---
+++ src/Ghassen/Demo.java
@@ -0,0 +1,9 @@
+package Ghassen;
+
+
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author GhassenKh
+ *
+ */
+module GitTuto {
+}
